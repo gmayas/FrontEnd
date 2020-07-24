0c6eb02acbb02912f89d7f43291f8d894848f7db
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2020-07-24 18:38:57 -0500

    commit FrontEnd

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/FrontEnd'));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/FrontEnd/index.html'))
 });
 
 app.listen(process.env.PORT || 8000);
